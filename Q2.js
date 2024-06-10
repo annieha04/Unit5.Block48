8aa162dbfc2e0b7aab2bdccd7c121c82e18c47c2
annieha04 <itsannha@gmail.com>
2024-06-10 18:02:13 -0400

    added answer

--- Q2.js
+++ Q2.js
@@ -1,8 +1,44 @@
 // Question 2:
 // There are n people in a line waiting to purchase tickets, with the 0th person at the front and the (n - 1)th person at the back. 
-
 // You are given a 0-indexed integer array of tickets of length n, where tickets[I] represents the number of tickets that the ith person wishes to purchase. 
-
 // Each person purchases a ticket in exactly one second. A person can only purchase one ticket at a time and must return to the end of the line (which happens instantly) to purchase additional tickets. If a person has no more tickets to buy, he or she will leave the line. 
-
 // Return the time spent for the individual at position k (0-indexed) to finish buying tickets. 
+
+// EXAMPLE: 
+// Input: tickets = [2,3,2], k = 2
+// Output: 6
+
+// tickets = [2, 3, 2], k = 2
+// 1st - Person at index 0 buys one of their two tickets. Time increments by 1
+//     [1,3,2]
+// 2nd - Person at index 1 buys one ticket of their three tickets. Time increments by 1
+//     [1, 2, 2]
+// 3rd - Person at index 2 buys one ticket of their two tickets. Time increments by 1
+//     [1, 2, 1]
+// 4th - Person at index 0 buys second ticket and finishes. Time increments by 1
+//     [0, 2, 1]
+// 5th - Person at index 1 buys second ticket. Time increments by 1
+//     [0, 1, 1]
+// 6th - Person at index 2 buys second ticket and finishes. Time increments by 1
+//     [0, 1, 0]
+// In total is 6 seconds.
+
+function timeRequiredToBuy(tickets, k) {
+    let time = 0;
+    let n = tickets.length;
+
+    while(tickets[k] > 0) {
+        for (let i = 0; i < n; i++) {
+            if (tickets[i] > 0) {
+                tickets[i]--;
+                tickets++;
+                if (i === k && tickets[k] === 0) {
+                    return time;
+                }
+            }
+        }
+    }
+    return time;
+}
+
+console.log(timeRequiredToBuy(tickets, k)); // Output is 6
--- Q3.js
+++ Q3.js
@@ -1,5 +1,29 @@
 // Question 3:
 // You are given an array heights representing the heights of students in a class. The students are standing in a line, and their heights are listed in the order they appear from left to right. The school wants to arrange the students in a non-decreasing order by their heights. However, when they rearrange the students, a few may end up in different positions from their original positions.
-
 // You need to write a function heightChecker(heights) that determines the minimum number of students who are not standing in the correct positions after the rearrangement. Implement the function heightChecker and return the minimum number of students who are not positioned correctly.
 
+// Input: heights = [1, 1, 4, 2, 1, 3]
+// Sorted = [1, 1, 1, 2, 3, 4]
+
+// Index 0: same - '1' vs '1'
+// Index 1: same - '1' vs '1'
+// Index 2: different - '4' vs '1'
+// Index 3: same - '2' vs '2'
+// Index 4: different - '1' vs '3'
+// Index 5: different - '3' vs '4'
+// Index 2, 4, 5 is dfferent, so output is 3
+
+
+function heightChecker(heights) {
+    const sortedHeights = [...heights].sort((a, b) => a - b);
+    let count = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== sortedHeights[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(heightChecker(heights)); // Output: 3
