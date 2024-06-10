76b0b25fe2aebc59555dfcfbac35c11ad72bda45
annieha04 <itsannha@gmail.com>
2024-06-10 17:31:39 -0400

    added q2, q3, q4

--- Q1.js
+++ Q1.js
@@ -1,3 +1,4 @@
+// Question 1:
 // Determine the validity of an input string s, which consists solely of the characters (, ), {, }, [, and ]. You need to check the following conditions: 
 
 //   The opening brackets: 
@@ -13,5 +14,25 @@ function isValid(s) {
     const stack = [];
     const openBrackets = ["(", "{", "["];
     const closingBrackets = [")", "}", "]"];
+
+    for (let char of s) {
+        if (openBrackets.includes(char)) {
+            stack.push(char);
+    } else {
+        const closingIndex = closingBrackets.indexOf(char);
+        const matchingOpenBracket = openBrackets[closingIndex];
+        
+        if (stack.length === 0 || stack.pop() !== matchingOpenBracket) {
+            return false;
+        }
+    }
+    }
+
+    return stack.length === 0;
 }
 
+const s1 = "()";
+console.log(isValid(s1)); // Output: true
+
+const s2 = "(){}[]";
+console.log(isValid(s2)); // Output: true
---
+++ Q4.js
@@ -0,0 +1,16 @@
+// Question 4:
+// You are given a deck of integer arrays. There is a deck of cards with each card having a unique integer, and the integer on the ith card is deck[i].  
+
+// You can arrange the deck in any order; all cards in one deck are initially face-down (unrevealed).  
+
+// Repeat the following steps until all cards are revealed.  
+
+// Step 1: Take out the top card from the deck and reveal it. 
+
+// Step 2: If there are still cards in the deck, you should place the next top card at the bottom of the deck. 
+
+// Step 3: If any cards remain unseen, return to step 1. Otherwise, proceed to the end. 
+
+// Step 4: Return a deck order that reveals the cards in increasing order. 
+
+
