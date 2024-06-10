5b29586c5c12249f9b19e07738adf8a9057c8966
annieha04 <itsannha@gmail.com>
2024-06-10 17:20:55 -0400

    first commit

---
+++ Q1.js
@@ -0,0 +1,17 @@
+// Determine the validity of an input string s, which consists solely of the characters (, ), {, }, [, and ]. You need to check the following conditions: 
+
+//   The opening brackets: 
+
+//   - Must match with the corresponding closing brackets of the same type 
+  
+//   - Must be closed in the correct order 
+  
+//   - Should have a corresponding opening bracket of the same type
+
+
+function isValid(s) {
+    const stack = [];
+    const openBrackets = ["(", "{", "["];
+    const closingBrackets = [")", "}", "]"];
+}
+
